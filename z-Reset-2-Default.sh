141fd29991eb650fa7d7b1ee0c15313353c390b9
DRok17 <81541725+DRok17@users.noreply.github.com>
2022-10-04 08:48:45 -0400

    Update z-Reset-2-Default.sh

--- z-Reset-2-Default.sh
+++ z-Reset-2-Default.sh
@@ -2,7 +2,9 @@
 css_path="/home/deck/.local/share/Steam/steamui/css/library.css"
 css_backupfile="./1boot/css-js/library.css"
 js_backupfile="./1boot/css-js/library.js"
+ovr_path="/home/deck/.local/share/Steam/steamui/overrides/movies/deck_startup.webm"
 rm $css_path
 rm $css_backupfile
 rm $js_backupfile
+rm $ovr_path
 qdbus org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
