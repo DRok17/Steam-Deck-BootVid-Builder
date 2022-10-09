f5b2059d4c994bd379aba6d72b0fbd43bc5473ad
DRok17 <81541725+DRok17@users.noreply.github.com>
2022-10-08 22:29:34 -0400

    Added Duration Change script
    JS files can be modified, but won't be stored in the css-js folder like before. If Valve fixes this, then I'll remove it.

---
+++ z-Change-Duration.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+js_path="/home/deck/.local/share/Steam/steamui/library.js"
+ten="./1boot/10.txt"
+twenty="./1boot/20.txt"
+thirty="./1boot/30.txt"
+
+if [ -e $ten ]
+then
+    #Get Stats
+    js_size=$(stat --printf="%s" $js_path)
+    old_duration_setting="s,1e4"
+    new_duration_setting="s,2e4"
+    sed -i -e"s/$old_duration_setting/$new_duration_setting/" $js_path
+    truncate -s $(($js_size)) $js_path
+    cd ./1boot
+    rm 10.txt
+    echo "twenty" > 20.txt
+    exit
+else
+    if [ -e $twenty ]
+    then
+        #Get Stats
+        js_size=$(stat --printf="%s" $js_path)
+        old_duration_setting="s,2e4"
+        new_duration_setting="s,3e4"
+        sed -i -e"s/$old_duration_setting/$new_duration_setting/" $js_path
+        truncate -s $(($js_size)) $js_path
+        cd ./1boot
+        rm 20.txt
+        echo "thirty" > 30.txt
+        exit
+    else
+       if [ -e $thirty ]
+       then
+            #Get Stats
+            js_size=$(stat --printf="%s" $js_path)
+            old_duration_setting="s,3e4"
+            new_duration_setting="s,1e4"
+            sed -i -e"s/$old_duration_setting/$new_duration_setting/" $js_path
+            truncate -s $(($js_size)) $js_path
+            cd ./1boot
+            rm 30.txt
+            echo "ten" > 10.txt
+            exit
+        else
+            cd ./1boot
+            echo "ten" > 10.txt
+            exit
+        fi
+    fi
+
+fi
+exit
